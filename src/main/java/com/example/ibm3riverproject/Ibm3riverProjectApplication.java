38428577f9c37d780d8b3fc901745db0cf3c8833
Daniel Unogwu <daniel.unogwu.external@atos.net>
2022-07-17 12:29:47 -0400

    Initial Project setup

---
+++ src/main/java/com/example/ibm3riverproject/Ibm3riverProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibm3riverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibm3riverProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ibm3riverProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ibm3riverproject/Ibm3riverProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibm3riverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ibm3riverProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
